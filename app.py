a481c3fbf352d47e0ab7638b02c2b01c027bc02a
Barbier--Darnal Joseph <79746670+JosephBARBIERDARNAL@users.noreply.github.com>
2023-01-05 16:12:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import re
+import os
 import pandas as pd
 from ipynb.fs.full.get_variables import get_all_variables
 from ipynb.fs.full.others import *
@@ -8,7 +9,30 @@ from ipynb.fs.full.output_df import list_to_df
 import PyPDF2
 
 
-st.title("Get the data from the [SHARE study](https://share-eric.eu) you want in 3 easy steps")
+
+
+
+
+
+#START
+
+st.title("Data exploration of the [SHARE study](https://share-eric.eu)")
+
+data_load_state = st.text("LOADING...")
+
+#get file directory
+st.markdown("Enter your working directory")
+full_path = st.text_input("example: /Users/josephbarbier/Desktop/PROJETpython/", '/Users/josephbarbier/Desktop/PROJETpython/')
+st.write("Current directory:", full_path)
+st.text("")
+st.markdown("***If you obtain the following error, your working directory is probably wrong. Verify that you did not change any file or folder name before using the app.***")
+st.code("FileNotFoundError: [Errno 2] No such file or directory:")
+
+st.text("")
+st.text("")
+st.text("")
+
+
 
 
 
@@ -20,10 +44,11 @@ st.title("Get the data from the [SHARE study](https://share-eric.eu) you want in
 #the app description
 st.sidebar.markdown("## The app")
 st.sidebar.markdown("In this app, you have the possibility to choose the variables from the [SHARE study](https://share-eric.eu) you are interested in at a given wave. From these, you can then retrieve a csv file containing these variables and the merge identifier (called 'mergeid' in SHARE).")
+st.sidebar.markdown("")
 
 #source code
 st.sidebar.markdown("## Source code")
-st.sidebar.markdown("You can fin the source code at this [Github](https://github.com/JosephBARBIERDARNAL)")
+st.sidebar.markdown("You can find the source code on this [Github repository](https://github.com/JosephBARBIERDARNAL/streamlitSHARE)")
 
 
 #about us description
@@ -39,15 +64,14 @@ st.sidebar.markdown("Contact: blabla@gmail.com")
 
 #MAIN
 
-data_load_state = st.text("LOADING...")
-
 
 #STEP 1: CHOOSE A WAVE
 
 #create wave full names
 years = ["2004", "2006/7", "2008/9", "2011", "2013", "2015", "2017", "2019/20"]
 waves = years[0]
-st.markdown("### Step 1: select a wave")
+st.markdown("## Step 1: select a wave")
+st.markdown("***The SHARE database is separated into 8 different waves of questionnaires spread over time. Choose the period or wave you are interested in before proceeding to the next step.***")
 waves = ["wave "+str(i)+"  (published in "+year+")" for i,year in zip(range(1,9), years)]
 
 #scrolling bar
@@ -55,21 +79,28 @@ wave = st.selectbox("Choose the wave you want to get the data from", waves)
 
 #output the wave choosen
 selected_wave = re.search(r'\d+', wave)
-suffix = selected_wave.group()
-st.write(f"You have selected the {th(int(suffix))} wave.")
-if int(suffix) not in [i for i in range(1,9)]:
+wave_chosen = int(selected_wave.group())
+st.write(f"You have selected the {th(wave_chosen)} wave.")
+if wave_chosen not in [i for i in range(1,9)]:
     print("There is a problem")
 
     
+st.text("")
+st.text("")
+st.text("")    
     
     
+    
+    
+    
+
 
 #STEP 2 : CHOOSE THE VARIABLES
 
 #get variables from the selected wave and scrolling bar
-st.markdown("### Step 2: select variables")
+st.markdown("## Step 2: select variables")
 variables = st.multiselect(f"Select variables from the choosen wave (max is 10)",
-                           get_all_variables(int(selected_wave.group())),
+                           get_all_variables(path=full_path, wave=wave_chosen),
                            max_selections=10)
 
 #output the choosen variables
@@ -79,63 +110,73 @@ list_var = list(variables)
 st.markdown("##### *You have selected the following variables:*")
 
 #path where to find the documentation of each wave
-path = f"/Users/josephbarbier/Desktop/PROJETpython/questionnaire/wave{suffix}questionnaire.pdf"
+path = f"{full_path}/questionnaire/wave{str(wave_chosen)}questionnaire.pdf"
+docu = extract_text_from_pdf(pdf_file=path)
 
-#if the wave choosen is in the wave 1 or 2, use this code
-if int(suffix) in [1, 2]:
-    documentation = remove_selected(extract_text_from_pdf(pdf_file=path))
-    for var in list_var:
-        clean_name = get_description_12(var.upper(), text_to_scrap=documentation)
-        st.write(var + ": "+change_case(clean_name, int(suffix)))
-
-#if the wave choosen is in the wave 4, 5 ,6, 7 or 8, use thise code
-if int(suffix) in [4,5,6,7,8]:
-    documentation = extract_text_from_pdf(pdf_file=path)
-    for var in list_var:
-        clean_name = get_description_45678(var.upper(), text_to_scrap=documentation)
-        st.write(var + ": "+change_case(clean_name, int(suffix)))
-
-#if the wave choosen is in the wave 3, use this code
-if int(suffix) == 3:
-    documentation = extract_text_from_pdf(pdf_file=path)
-    for var in list_var:
-        clean_name = get_description_3(var.upper(), text_to_scrap=documentation)
-        st.write(var + ": "+change_case(clean_name, int(suffix)))
+#use different function to get the description of the variables chosen depending on the wave
+for var in list_var:
+    if wave_chosen in [1, 2]:
+        docu = remove_selected(docu)
+        clean_name = get_description_12(var.upper(), text_to_scrap=docu)
+    if wave_chosen in [4,5,6,7,8]:
+        clean_name = get_description_45678(var.upper(), text_to_scrap=docu)
+    if wave_chosen == 3:
+        clean_name = get_description_3(var.upper(), text_to_scrap=docu)
+    
+    #print the variables chosen and their full name
+    st.write(var + ": "+change_case(clean_name, wave_chosen))
 
 
-        
-        
+st.text("")
+st.text("")
+st.text("")        
+
+
+
+
+
+
+
+
 #STEP 3: RETRIEVE YOUR DATA!
 
-st.markdown("### Step 3: get the data")
+st.markdown("## Step 3: get the data")
 
 #add the mergeid and the country, and transform the list into a df
 list_var.extend(["mergeid","country"])
-data = list_to_df(wave=int(suffix), variables=list_var)
+data = list_to_df(wave=wave_chosen, variables=list_var, path=full_path)
+row, col = data.shape
+
+#print shape of the df
+st.markdown("***Shape of the current dataset***")
+st.write(f"Sample size: {row}. Number of cols: {col}")
+#st.write(f"Number of cols: {col}")
+
+#print some informations about the variables chosen
 if len(list_var) > 2:
+    
+    #print head of the df
     st.dataframe(data.head())
-
-#print NA percentage of each variable
-for var in data:
-    tot_na = data[var].isna().sum()
-    na_percent = round(tot_na/len(data)*100,2)
-    st.write(f"Percentage of NAs from the variable {data[var].name}: {na_percent}%\n")
-
-
+    
+    st.markdown("***Percentage of NAs of each variable:***")
+    for var in data:
+        tot_na = data[var].isna().sum()
+        na_percent = round(tot_na/len(data)*100,2)
+        st.write(f"{data[var].name}: {na_percent}%")
+    
+    #return the df cleaned
+    df_to_return = data.to_csv().encode('utf-8')
+    st.download_button(label="Dowload the dataset",
+                       data=df_to_return,
+                       file_name = "my_share_data.csv")
+    
     
 
 
-#return the df cleaned
-df_to_return = data.to_csv().encode('utf-8')
-#df_to_return = df_to_return.dropna(axis=1, how='all')
-st.download_button(label="Dowload the dataset",
-                   data=df_to_return,
-                   file_name = "my_share_data.csv")
 
+data_load_state.text("Loading ended!")
 
-data_load_state.text("Data loaded!")
-
-
+st.markdown("***Selectionnez le graphique ***")
 
 
 
